34599207279c2efbd5a0cc97568715a588962058
RutamBhagat <rutambhagat@gmail.com>
2023-06-10 15:36:47 +0530

    fix schema problem

--- prisma/migrations/20230610093604_temp/migration.sql
+++ prisma/migrations/20230610093604_temp/migration.sql
@@ -4,7 +4,7 @@ CREATE TABLE "User" (
     "name" TEXT DEFAULT 'Anonymous',
     "email" TEXT NOT NULL,
     "password" TEXT NOT NULL,
-    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "created_at" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
     "updated_at" TIMESTAMP(3) NOT NULL,
 
     CONSTRAINT "User_pkey" PRIMARY KEY ("id")
---
+++ prisma/seed.ts
@@ -0,0 +1,43 @@
+import { PrismaClient } from "@prisma/client";
+
+const prisma = new PrismaClient();
+
+async function main() {
+  await prisma.user.deleteMany();
+
+  const userLaith = await prisma.user.create({
+    data: {
+      email: "laith@gmail.com",
+      password: "$2b$10$I8xkU2nQ8EAHuVOdbMy9YO/.rSU3584Y.H4LrpIujGNDtmny9FnLu",
+    },
+  });
+
+  const userJosh = await prisma.user.create({
+    data: {
+      email: "josh@gmail.com",
+      password: "$2b$10$I8xkU2nQ8EAHuVOdbMy9YO/.rSU3584Y.H4LrpIujGNDtmny9FnLu",
+    },
+  });
+
+  const userLebron = await prisma.user.create({
+    data: {
+      email: "lebron@gmail.com",
+      password: "$2b$10$I8xkU2nQ8EAHuVOdbMy9YO/.rSU3584Y.H4LrpIujGNDtmny9FnLu",
+    },
+  });
+
+  const userCassidy = await prisma.user.create({
+    data: {
+      email: "cassidy@gmail.com",
+      password: "$2b$10$I8xkU2nQ8EAHuVOdbMy9YO/.rSU3584Y.H4LrpIujGNDtmny9FnLu",
+    },
+  });
+  console.log("Seed successful");
+}
+main()
+  .then(() => prisma.$disconnect())
+  .catch(async (e) => {
+    console.error(e);
+    await prisma.$disconnect();
+    process.exit(1);
+  });
