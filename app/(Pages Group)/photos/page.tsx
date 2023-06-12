2137ce6ed37ba108fefadd61024476b57ffeb18c
RutamBhagat <rutambhagat@gmail.com>
2023-06-12 12:34:37 +0530

    move getPhotos and getPosts logic to server components from route handlers

--- app/(Pages Group)/photos/page.tsx
+++ app/(Pages Group)/photos/page.tsx
@@ -1,24 +1,38 @@
 import { type Photo } from "@prisma/client";
-import axios from "axios";
 import ReactMasonaryComponent from "./components/ReactMasonary";
 import CreatePhoto from "./components/CreatePhoto";
 import { getServerSession } from "next-auth";
 import { authOptions } from "@/lib/auth";
+import { prisma } from "@/lib/prisma";
 
 export const revalidate = 0;
 
+const getPhotos = async (user_id: string) => {
+  try {
+    const data: Photo[] = await prisma.photo.findMany({
+      where: {
+        userId: user_id,
+      },
+      include: {
+        user: true,
+      },
+      orderBy: {
+        createdAt: "asc",
+      },
+    });
+    return data;
+  } catch (error) {
+    console.log("error", error);
+  }
+};
+
 export default async function page() {
   const session = await getServerSession(authOptions);
-  const config = {
-    headers: {
-      Authorization: `Bearer ${session?.user.accessToken}`,
-    },
-  };
-  const { data } = await axios.get<Photo[]>(`${process.env.BASE_URL}/api/photos/getPhotos`, config);
+  const data = await getPhotos(session?.user?.id as string);
   return (
     <>
       <div className="flex-1 p-3">
-        <ReactMasonaryComponent data={data} />
+        <ReactMasonaryComponent data={data as Photo[]} />
       </div>
       <CreatePhoto />
     </>
--- app/(Pages Group)/text/page.tsx
+++ app/(Pages Group)/text/page.tsx
@@ -1,20 +1,34 @@
-import { Post } from "@prisma/client";
+import { type Post } from "@prisma/client";
 import React from "react";
 import Posts from "./components/Posts";
-import axios from "axios";
 import { getServerSession } from "next-auth";
 import { authOptions } from "@/lib/auth";
+import { prisma } from "@/lib/prisma";
 
 export const revalidate = 0;
 
+const getTexts = async (user_id: string) => {
+  try {
+    const data: Post[] = await prisma.post.findMany({
+      where: {
+        userId: user_id,
+      },
+      include: {
+        user: true,
+      },
+      orderBy: {
+        createdAt: "asc",
+      },
+    });
+    return data;
+  } catch (error) {
+    console.log("error", error);
+  }
+};
+
 export default async function page() {
   const session = await getServerSession(authOptions);
-  const config = {
-    headers: {
-      Authorization: `Bearer ${session?.user.accessToken}`,
-    },
-  };
-  const { data } = await axios.get<Post[]>(`${process.env.BASE_URL}/api/posts/getPosts`, config);
+  const data = await getTexts(session?.user?.id as string);
   return (
     <>
       {data?.map((post) => (
