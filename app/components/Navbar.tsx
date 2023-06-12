154be0f43a5a45ada335fab848ef87909fc74225
RutamBhagat <rutambhagat@gmail.com>
2023-06-12 18:12:00 +0530

    remodify navbar

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -74,6 +74,7 @@ export default function Navbar() {
               Register
             </Link>
             <SignInButton />
+            {/* <SessionButton /> */}
           </div>
         </div>
       </div>
